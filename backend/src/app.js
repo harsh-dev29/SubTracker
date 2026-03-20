8ba16a4e28a42fa0b09e7d9fdb0a2d548bf51779
harsh <harshvishnoioffcial@gmail.com>
2026-03-20 18:23:25 +0530

    backend cors origin changed

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,7 +10,7 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://sub-tracker-one.vercel.app/",
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
