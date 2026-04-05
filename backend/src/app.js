c2eb15d618da04b4509286655e734cc061db8005
harsh <harshvishnoioffcial@gmail.com>
2026-04-05 13:21:37 +0530

    origin changed to normal

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,13 +14,7 @@ const allowedOrigins = [
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-            callback(null, true)
-        } else {
-            callback(new Erro("Not allowed by CORS"))
-        }
-    },
+    origin: 'https://sub-tracker-one.vercel.app',
     credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE']
 }))
